906b2cafb86e3bf101262c17cd8be7dd6c93ad16
rosewww <72166917+rosewww@users.noreply.github.com>
2022-03-03 16:36:20 +0100

    end of pom.xml

---
+++ projetmaven/src/test/java/tp10/metier/ActionCompliqueeTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tp10.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 86136
+ */
+public class ActionCompliqueeTest {
+    
+    public ActionCompliqueeTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+    }
+    
+}
